9eeef5d8818685f531b05bd9f25ca7f7324aed02
Annanikol <annanikol.kam@gmail.com>
2025-10-05 21:42:20 +0000

    Fix: base path, CSS подключен, контент отображается

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
 
-  server: {
-    host: "::", // позволяет доступ с любого IPv6-адреса
-    port: 8080, // порт для локального сервера
-  },
-  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // позволяет использовать "@/components/..." вместо длинных путей
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+
   build: {
-    outDir: "dist", // папка для финальной сборки
-    emptyOutDir: true, // очищает папку перед новой сборкой
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+
+  plugins: [react()],
+
+  server: {
+    host: "::",
+    port: 8080,
   },
 });
