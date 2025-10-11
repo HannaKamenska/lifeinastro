e87f3685f4cc01a503cd3ad85d09d4849ab4a3ec
Annanikol <annanikol.kam@gmail.com>
2025-10-11 17:29:38 +0000

    Обновления

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,17 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: "/",
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@test": path.resolve(__dirname, "./tests"),
     },
   },
-
   build: {
     outDir: "dist",
     emptyOutDir: true,
   },
-
   plugins: [react(), tsconfigPaths()],
-
   server: {
     host: "::",
     port: 8080,
